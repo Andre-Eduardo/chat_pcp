4538004dcdb978b5986ecdfa63daf64432fbb8df
Andre Eduardo <andre.eduardo11@hotmail.com>
2023-03-17 22:56:41 -0300

    configurando scroll

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import 'react-chat-elements/dist/main.css'
+import './styles.css'
 import {
   Avatar,
   Input,
@@ -7,21 +8,24 @@ import {
   MessageBox,
   ChatItem,
 } from 'react-chat-elements'
-import { ScrollView } from 'react-native-web'
-import { IoMdSearch } from 'react-icons/io'
 
+import { IoMdSearch } from 'react-icons/io'
+import { IoPaperPlane } from 'react-icons/io5'
 export default function Chat() {
   return (
     <div className="flex justify-center  h-[100vh] bg-[#F2F2F2] ">
-      <div className="container  max-w-[43rem] bg-[#E4E4E4] flex flex-col items-center justify-between">
-        <section className="fixed h-[4.6rem] bg-white w-full max-w-[43rem] flex flex-row  justify-between items-center px-7">
+      <div className="container   max-w-[43rem] bg-[#E4E4E4] flex flex-col items-center justify-between">
+        <section className=" fixed z-50 h-[4.6rem] bg-white w-full max-w-[43rem] flex flex-row  justify-between items-center px-7">
           <div className="flex flex-row">
-            <Avatar
-              src="https://avatars.githubusercontent.com/u/80540635?v=4"
-              alt="avatar"
-              size="xlarge"
-              type="rounded"
-            />
+            <div className="relative">
+              <Avatar
+                src="https://avatars.githubusercontent.com/u/80540635?v=4"
+                alt="avatar"
+                size="xlarge"
+                type="rounded"
+              />
+              <div className="absolute w-3 h-3 bg-[#47DC44] rounded-full top-9 right-1"></div>
+            </div>
             <div className="ml-3">
               <h3 className="text-black font-semibold text-lg">
                 Caroline Carvalho
@@ -31,113 +35,122 @@ export default function Chat() {
               </h4>
             </div>
           </div>
-          <IoMdSearch />
-        </section>
-        <section className="h-full w-full">
-          <div className="bg-black w-full h-full ">
-            <ScrollView>
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-              <MessageBox
-                // @ts-ignore
-                position="left"
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                  uri: 'https://facebook.github.io/react/img/logo.svg',
-                  status: {
-                    click: false,
-                    loading: 0,
-                  },
-                }}
-              />
-            </ScrollView>
+          <div className="bg-[#1AA7BB] w-8 h-8 flex items-center justify-center rounded-full">
+            <IoMdSearch size={20} color="#fff" />
           </div>
         </section>
-        <footer className="max-w-[43rem] mb-3 mx-5 w-full fixed bottom-0   ">
-          <Input
-            maxHeight={1}
-            className="h-[3.75rem] rounded-xl "
-            placeholder="Digite aqui."
-            multiline={true}
-            inputStyle={{ paddingLeft: 12, fontSize: 16 }}
-          />
+        <section className="h-[100vh] w-full overflow-y-scroll  ">
+          <div className=" z-10 w-full mt-[90px]    ">
+            <MessageBox
+              // @ts-ignore
+              position="right"
+              type={'text'}
+              className="bg-black"
+              styles={{ backgroundColor: 'green' }}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+            <MessageBox
+              // @ts-ignore
+              position="left"
+              type={'photo'}
+              text={'react.svg'}
+              data={{
+                uri: 'https://facebook.github.io/react/img/logo.svg',
+                status: {
+                  click: false,
+                  loading: 0,
+                },
+              }}
+            />
+          </div>
+        </section>
+        <footer className="max-w-[43rem]  md:px-5 w-full fixed z-50 bottom-0 bg-[#E4E4E4] ">
+          <div className="md:mb-3">
+            <Input
+              maxHeight={100}
+              className="h-[3.75rem] rounded-xl "
+              placeholder="Digite aqui."
+              // multiline={true}
+              rightButtons={
+                <div className="bg-[#4784DE] w-8 h-8 flex items-center justify-center rounded-full mr-3">
+                  <IoPaperPlane size={15} color="#fff" />
+                </div>
+              }
+              inputStyle={{ paddingLeft: 12, fontSize: 16 }}
+            />
+          </div>
         </footer>
       </div>
     </div>
