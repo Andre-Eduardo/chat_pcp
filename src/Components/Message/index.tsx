8349d238674770e6f854cb832c5f8a8281ff9799
Andre Eduardo <andre.eduardo11@hotmail.com>
2023-07-21 22:04:37 -0300

    ajuste quebra de linha

--- src/Components/Message/index.tsx
+++ src/Components/Message/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { DateFormatted } from '../../Functions/DateFormatted'
 import { DateApi } from '../../Functions/DateApi'
 import './styles.scss'
@@ -14,16 +14,40 @@ interface propsMessageData {
 }
 
 export default function Message(data: propsMessageData) {
+  const divRef = useRef(null)
+
+  useEffect(() => {
+    const div = divRef.current as any
+
+    if (div && div.scrollWidth > div.clientWidth) {
+      const palavras = data.Mensagem.split(' ')
+      let textoFormatado = ''
+      let linhaAtual = ''
+
+      palavras.forEach((palavra) => {
+        if (linhaAtual.length + palavra.length + 1 <= div.clientWidth) {
+          linhaAtual += palavra + ' '
+        } else {
+          textoFormatado += linhaAtual.trim() + '<br />'
+          linhaAtual = palavra + ' '
+        }
+      })
+
+      textoFormatado += linhaAtual.trim()
+      div.innerHTML = textoFormatado
+    }
+  }, [data.Mensagem])
+
   return (
     <>
       <div ref={data.reference} className=" w-full flex  mt-2">
         <div
-          className={` max-w-[95%] px-4 pt-4 pb-2  rounded-r-3xl rounded-b-3xl ml-3 flex  items-center ${
+          className={` w-auto px-4 pt-4 pb-2  rounded-r-3xl rounded-b-3xl ml-3 flex  items-center ${
             data.Sender ? 'bg-[#FFFFFF]' : 'bg-[#4784DE]'
           }`}
         >
-          <div className=" mr-5 max-w-[100%]">
-            <div className="flex flex-col">
+          <div className=" mr-5 w-auto">
+            <div className="flex flex-col w-auto ">
               <p
                 className={`
                 capitalize  mr-1
@@ -31,15 +55,20 @@ export default function Message(data: propsMessageData) {
               >
                 {`${data.TipoUsuario}`}:
               </p>
-              <p
-                className={`font-normal text-left  break-long-words  ${
-                  data.Sender
-                    ? 'text-[#121212] opacity-50'
-                    : 'text-[#fff] opacity-70'
-                }`}
-              >
-                {data.Mensagem}
-              </p>
+              <div className="over">
+                <p
+                  ref={divRef}
+                  className={`font-normal  break-long-words 
+                 ${
+                   data.Sender
+                     ? 'text-[#121212] opacity-50'
+                     : 'text-[#fff] opacity-70'
+                 }
+                `}
+                >
+                  {data.Mensagem}
+                </p>
+              </div>
             </div>
             <h4
               className={` ${
